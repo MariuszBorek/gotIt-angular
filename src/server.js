ad4ca7a481885cd5ef8abe1181a0d08a7846b5c4
Mariusz Borek <mariuszborek242@gmail.com>
2021-01-26 00:16:36 +0100

    changed server.js

--- src/server.js
+++ src/server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/GOTIT-FORNTEND'));
+app.use(express.static('./dist/gotit-frontend'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', { root: 'dist/GOTIT-FORNTEND/' }),
+    res.sendFile('index.html', { root: 'dist/gotit-frontend/' }),
 );
 
 // Start the app by listening on the default Heroku port
