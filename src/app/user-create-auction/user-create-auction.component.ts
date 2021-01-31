ecab3f540fbb2914489eddb13bdbeb16ade9005b
Mariusz Borek <mariuszborek242@gmail.com>
2021-01-31 17:32:47 +0100

    form validation in create auction site

--- src/app/user-create-auction/user-create-auction.component.ts
+++ src/app/user-create-auction/user-create-auction.component.ts
@@ -13,15 +13,19 @@ import { UserProfileService } from '../service/user-profile.service';
 })
 export class UserCreateAuctionComponent implements OnInit {
 
-  newCreatedAuction = new NewAuctionDTO('', '', '', '', '', false, 0, false);
+  newCreatedAuction = new NewAuctionDTO('', '', '', '0', '', false, 7, false);
   categories: CategoryDTO[];
   selectedFile: File;
 
-  constructor(private homeService: HomeService, private userProfileService: UserProfileService) { }
+  messagePhoto: string;
+  messageFormData: string;
+  messageCreatedAuction: string;
+
+  constructor(private homeService: HomeService, private userProfileService: UserProfileService, private router: Router) { }
 
   getCategory() {
     this.homeService.getCategories()
-      .subscribe(categories => { this.categories = categories, console.log(categories); });
+      .subscribe(categories => this.categories = categories);
   }
 
   public onFileChanged(event) {
@@ -30,10 +34,39 @@ export class UserCreateAuctionComponent implements OnInit {
 
   onUploadAuction() {
     const uploadImageData = new FormData();
-    uploadImageData.append('imageFile', this.selectedFile, this.selectedFile.name);
-    this.userProfileService.createUserAuction(this.newCreatedAuction, uploadImageData).subscribe(res => console.log('ok'));
-
+    if(this.checkIfPhotoWasAdded() && this.checkIfBasicFormDataAreFilled()) {
+      console.log(this.newCreatedAuction);
+      uploadImageData.append('imageFile', this.selectedFile, this.selectedFile.name);
+      this.userProfileService.createUserAuction(this.newCreatedAuction, uploadImageData).subscribe(() => {
+        this.messageCreatedAuction = 'success!';
+        this.router.navigate(['user-auctions']);
+      });
+    }
   }
+
+  checkIfPhotoWasAdded(): boolean {
+    if(typeof this.selectedFile === 'undefined') {
+      this.messagePhoto = 'Add photo';
+      return false;
+    } else {
+      this.messagePhoto = '';
+      return true;
+    }
+  }
+
+  checkIfBasicFormDataAreFilled(): boolean {
+    if(this.newCreatedAuction.category != '' &&
+    this.newCreatedAuction.category != 'Choose category' &&
+    this.newCreatedAuction.title != '' &&
+    this.newCreatedAuction.buyNowPrice!= '') {
+      this.messageFormData = '';
+      return true;
+    } else {
+      this.messageFormData = 'category, title and buy now price is mandatory';
+      return false;
+    }
+  }
+
 
   refresh(): void {
     window.location.reload();
